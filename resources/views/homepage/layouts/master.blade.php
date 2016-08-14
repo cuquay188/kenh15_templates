9a47667c3d5a3dec14e4c8c146be8fbdf29716f1
Trí Phạm <pvtri96@gmail.com>
2016-08-14 17:17:13 +0700

    fix master

--- resources/views/homepage/layouts/master.blade.php
+++ resources/views/homepage/layouts/master.blade.php
@@ -10,20 +10,13 @@
     <link rel="stylesheet" href="{{asset('/css/homepage/app.css')}}">
     <link rel="stylesheet" href="{{asset('/css/homepage/sidebar-left.css')}}">
 
-@yield('styles')
-<!-- jQuery library -->
+    @yield('styles')
+
+    <!-- jQuery library -->
     <script src="{{asset('/js/jquery-2.2.4.min.js')}}"></script>
 
     <!-- Latest compiled JavaScript -->
     <script src="{{asset('/bootstrap/js/bootstrap.min.js')}}"></script>
-
-    <link rel="stylesheet" type="text/css" href="{{asset('/css/datatables.min.css')}}"/>
-
-    <script type="text/javascript" src="{{asset('/js/datatables.min.js')}}"></script>
-
-    {{--<script src="//cdn.ckeditor.com/4.5.9/full/ckeditor.js"></script>--}}
-
-    <script type="text/javascript" src="{{asset('/ckeditor/ckeditor.js')}}"></script>
 
     @yield('scripts')
 </head>
