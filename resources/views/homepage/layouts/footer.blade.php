b757dc6f4409e4a5e431c2a98a093e5b77e35933
dungle1811 <dungle1811@gmail.com>
2016-08-24 00:19:16 +0700

    - fixed footer.

--- resources/views/homepage/layouts/footer.blade.php
+++ resources/views/homepage/layouts/footer.blade.php
@@ -3,17 +3,9 @@
         <div class="categories">
             <ul>
                 @foreach(\App\Category::all() as $category)
-                    <li class="col-xs-2"><a href="#">{{$category->name}}</a></li>
+                    <li><a href="{{route('homepage').'/category/'.$category->id}}">{{$category->name}}</a></li>
                 @endforeach
             </ul>
-        </div>
-        <div class="info">
-            <div class="col-xs-2">
-                <a href="#">About Us</a>
-            </div>
-            <div class="col-xs-2">
-                <a href="#">Terms Use</a>
-            </div>
         </div>
     </div>
 </footer>
