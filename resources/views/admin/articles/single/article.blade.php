149f5d3afc819c876e1f07922412dbe58da67511
Trí Phạm <pvtri96@gmail.com>
2016-08-24 16:30:58 +0700

    fix categories management. add function getContentHeader in Article Model

--- resources/views/admin/articles/single/article.blade.php
+++ resources/views/admin/articles/single/article.blade.php
@@ -27,51 +27,17 @@
             ?>
         </div>
         <div class="authors border-tag">
-            <?php
-            $authors = $article->authors;
-            $authors_filter = array();
-            if (!function_exists('search_author')) {
-                function search_author($author_id, $authors)
-                {
-                    foreach ($authors as $author) {
-                        if ($author->id == $author_id) return true;
-                    }
-                    return false;
-                }
-            }
-            foreach ($authors as $author) {
-                if (!search_author($author->id, $authors_filter))
-                    array_push($authors_filter, $author);
-            }
-            ?>
             <p>
                 <label>Author(s):</label>
-                @foreach($authors_filter as $author)
+                @foreach($article->authors as $author)
                     <a href="{{route('author').'/'.$author->id}}">{{$author->user->name}}</a>
                 @endforeach
             </p>
         </div>
         <div class="tags border-tag">
-            <?php
-            $tags = $article->tags;
-            $tags_filter = array();
-            if (!function_exists('search_tag')) {
-                function search_tag($tag_id, $tags)
-                {
-                    foreach ($tags as $tag) {
-                        if ($tag->id == $tag_id) return true;
-                    }
-                    return false;
-                }
-            }
-            foreach ($tags as $tag) {
-                if (!search_tag($tag->id, $tags_filter))
-                    array_push($tags_filter, $tag);
-            }
-            ?>
             <p>
                 <label>Tag(s):</label>
-                @foreach($tags_filter as $tag)
+                @foreach($article->tags as $tag)
                     <a href="{{route('tag').'/'.$tag->id}}">{{$tag->name}}</a>
                 @endforeach
             </p>
