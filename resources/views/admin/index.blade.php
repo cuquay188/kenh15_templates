f393c0c717425249ee8ccd387cf86e035bb40485
Trí Phạm <pvtri96@gmail.com>
2016-09-06 22:00:47 +0700

    move admin index page set href script to body script

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,5 +2,7 @@
 @section("title",'Dashboard')
 @section('content')
     <ng-view></ng-view>
+@endsection
+@section('body.scripts')
     <script>if (!location.hash)location.hash = '#/dashboard'</script>
 @endsection
