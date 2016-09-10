5a14004228e5db31d8b301c797dcedcec9a9657f
Trí Phạm <pvtri96@gmail.com>
2016-09-10 10:18:14 +0700

    add preview at homepage option in article management

--- resources/views/admin/articles/list/components/article.blade.php
+++ resources/views/admin/articles/list/components/article.blade.php
@@ -2,9 +2,15 @@
     <div class="modal-dialog wide">
         <div class="modal-content" ng-controller="editArticleController">
             <div class="modal-header">
-                <h5>%%article.title%%</h5>
+                <a class="btn btn-success" href="{{route('homepage')}}/article/%%article.url%%" target="_blank">
+                    Preview on Homepage
+                </a>
+                <h5 style="margin-left: 180px">
+                    %%article.title%%
+                </h5>
                 <div class="close" data-dismiss="modal" ng-click="dismiss()">
-                    <div class="glyphicon glyphicon-remove"></div>
+                    <div class="glyphicon glyphicon-remove">
+                    </div>
                 </div>
             </div>
             <div class="modal-body">
@@ -13,9 +19,15 @@
             </div>
             <div class="modal-footer">
                 <div class="form-group">
-                    <button class="btn btn-default" data-dismiss="modal" ng-click="dismiss()">Close</button>
-                    <button class="btn btn-danger" data-dismiss="modal" data-toggle="modal" data-target="#delete-article">Remove this article</button>
-                    <button class="btn btn-primary" data-dismiss="modal" data-toggle="modal" data-target="#update-article">Edit this article</button>
+                    <button class="btn btn-default" data-dismiss="modal" ng-click="dismiss()">
+                        Close
+                    </button>
+                    <button class="btn btn-danger" data-dismiss="modal" data-target="#delete-article" data-toggle="modal">
+                        Remove this article
+                    </button>
+                    <button class="btn btn-primary" data-dismiss="modal" data-target="#update-article" data-toggle="modal">
+                        Edit this article
+                    </button>
                 </div>
             </div>
         </div>
