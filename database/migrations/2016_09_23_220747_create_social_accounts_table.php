be7e30e5309e13031e0f7d7fd50072c9ad760acc
Trí Phạm <pvtri96@gmail.com>
2016-09-23 23:11:18 +0700

    alter onDelete cascade for each user in social_accounts

--- database/migrations/2016_09_23_220747_create_social_accounts_table.php
+++ database/migrations/2016_09_23_220747_create_social_accounts_table.php
@@ -13,7 +13,8 @@ class CreateSocialAccountsTable extends Migration
     public function up()
     {
         Schema::create('social_accounts', function (Blueprint $table) {
-            $table->integer('user_id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->string('provider_user_id');
             $table->string('provider');
             $table->timestamps();
